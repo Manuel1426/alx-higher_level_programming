712ef92dcbe451ff62b53e3e5d7396c5e53392e0
Manuel1426 <immaosas@gmail.com>
2023-12-03 07:01:52 -0800

    Python - Data Structures

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,72 +1,26 @@
-#include "lists.h"
-
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
-/**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
-
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
+#include <Python.h>
+#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: python object.
  */
-int is_palindrome(listint_t **head)
+void print_python_list_info(PyObject *p)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+	int size;
+	int allocated;
+	int i;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+	size = Py_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
 
-	tmp = *head;
-	while (tmp)
+	for (i = 0; i < size; i++)
 	{
-		size++;
-		tmp = tmp->next;
+		printf("Element %d: ", i);
+		printf("%s\n", ((PyList_GetItem(p, i))->ob_type)->tp_name);
 	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
-
-	return (1);
 }
